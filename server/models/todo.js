d68171fbeff67926bbc7a71a5fdda3dcbe4b2413
timmycron <timmycron@hotmail.com>
2016-12-21 11:55:11 -0800

    Add POST /todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+// mongoDB objects already have timestamps when created so we
+// don't need to include createdAt
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo};
