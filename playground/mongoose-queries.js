1f4b883ce03067a5e6adcce896c42d91661ce08c
timmycron <timmycron@hotmail.com>
2016-12-21 14:02:29 -0800

    Add GET /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '585ae36ca87630fa92cdaf72';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo by id', todo);
+});
