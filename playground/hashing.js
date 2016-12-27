ed04f7f664a1eeffd09e1a932ef89035d8da02dc
timmycron <timmycron@hotmail.com>
2016-12-27 12:25:38 -0800

    Hash user password bcrypt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc!';
 
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+// var data = {
+//   id: 10
+// }
+//
 // 123abc is the secret to be salted
-var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
-
+// var token = jwt.sign(data, '123abc');
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+//
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
